f77e28008fe5626001e88477f6568e3fcbb4aa90
YoHan <sscok66@naver.com>
2021-08-12 13:46:12 +0900

    요한이형 테스트

---
+++ Dteam_Projcet/src/main/java/Dteam_Projcet/basket/controller/BasketController.java
@@ -0,0 +1,5 @@
+package Dteam_Projcet.basket.controller;
+
+public class BasketController {
+	// 요한이형 테스트
+}
---
+++ Dteam_Projcet/src/test/java/Dteam_Projcet/DteamProjcetApplicationTests.java
@@ -0,0 +1,13 @@
+package Dteam_Projcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DteamProjcetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
